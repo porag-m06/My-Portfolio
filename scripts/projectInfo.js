73f355e6665c1dea4a895e4f5ca084b97318b33d
porag-m06 <porag.m06@gmail.com>
2023-04-20 23:26:07 +0600

    script for array of project details objects added

---
+++ scripts/projectInfo.js
@@ -0,0 +1,33 @@
+const projects = [
+    {
+      projectImgSrc: '../assets/bigger-screen/SnapshootPortfolio1.svg',
+      titleText: 'Tonic',
+      shortInfoDes: ['CANOPY', 'Back End Dev', '2015'],
+      DescriptionIntro: 'A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required.',
+      techTags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      projectImgSrc: '../assets/bigger-screen/SnapshootPortfolio2.svg',
+      titleText: 'Multi-Post Stories',
+      shortInfoDes: ['FACEBOOK', 'Full Stack Dev', '2015'],
+      DescriptionIntro: 'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+      techTags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      projectImgSrc: '../assets/bigger-screen/SnapshootPortfolio3.svg',
+      titleText: 'Facebook 360',
+      shortInfoDes: ['FACEBOOK', 'Full Stack Dev', '2015'],
+      DescriptionIntro: 'A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required.',
+      techTags: ['HTML', 'CSS', 'JavaScript'],
+    },
+    {
+      projectImgSrc: '../assets/bigger-screen/Snap4.svg',
+      titleText: 'Tonic',
+      shortInfoDes: ['CANOPY', 'Back End Dev', '2015'],
+      DescriptionIntro: 'A daily selection of privately <br> personalized reads; no accounts or <br> sign-ups required.',
+      techTags: ['HTML', 'CSS', 'JavaScript'],
+    },
+  ];
+  
+  export default projects;
+  
