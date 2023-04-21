09963ac92051e3248c1a987ff7428afcff71fd4a
porag-m06 <porag.m06@gmail.com>
2023-04-21 16:41:53 +0600

    resolved conflict

--- scripts/script.js
+++ scripts/script.js
@@ -122,3 +122,28 @@ for (let i = 0; i < projects.length; i += 1) {
 }
 
 // -------- details popup  window ENDS --------
+
+// -------- contact email validation STARTS --------
+
+const form = document.querySelector('form');
+const eInput = document.querySelector('#emil');
+let isLower;
+
+eInput.addEventListener('input', () => {
+  if (eInput.value === eInput.value.toLowerCase()) {
+    eInput.classList.remove('warn');
+    document.getElementById('demo').innerHTML = '';
+  }
+});
+
+form.addEventListener('submit', (event) => {
+  isLower = eInput.value === eInput.value.toLowerCase();
+
+  if (!isLower) {
+    event.preventDefault();
+    eInput.className = 'warn';
+    document.getElementById('demo').innerHTML = 'Email address must be in lowercase!';
+  }
+});
+
+// -------- contact email validation ENDS --------
