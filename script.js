e0d7033bb96f572c69b7bf3e427ce3ea0f2b66ae
porag-m06 <porag.m06@gmail.com>
2023-04-21 15:47:44 +0600

    script updated for contact form validation

--- script.js
+++ script.js
@@ -23,3 +23,28 @@ for (let i = 0; i < menuLinks.length; i += 1) {
   });
 }
 // -------- mobile menu navigation ENDS --------
+
+// -------- contact email validation STARTS --------
+
+const form = document.querySelector('form');
+const eInput = document.querySelector("#emil");
+let isLower;
+
+eInput.addEventListener("input", (event) => {
+  if(eInput.value === eInput.value.toLowerCase()){
+    eInput.classList.remove("warn");
+    document.getElementById("demo").innerHTML = "";
+  }
+})
+
+form.addEventListener("submit", (event) => {
+  isLower = eInput.value === eInput.value.toLowerCase();
+
+  if (!isLower) {
+    event.preventDefault();
+    eInput.className = 'warn';
+    document.getElementById("demo").innerHTML = "Email address must be in lowercase!";
+  }
+});
+
+// -------- contact email validation STARTS --------
